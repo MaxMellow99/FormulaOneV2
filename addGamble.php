0a28636640b33535929c913969c0e26461123c7c
Bendt <Bendtkaars@hotmail.com>
2022-06-24 00:07:37 +0200

    werkendpuntensysteemenophaalsysteem

--- addGamble.php
+++ addGamble.php
@@ -1,56 +1,64 @@
 <?php
-    session_start();
-
-    var_dump($_SESSION['id']);
-    require_once "managers/GambleManager.php";
-    require_once "managers/DriverManager.php";
+session_start();
 
 
-    if($_POST) {
-        //Gooit sortingtable leeg
-        GambleManager::ClearSorting();
-    }
+require_once "managers/GambleManager.php";
+require_once "managers/DriverManager.php";
+
+
+var_dump($_SESSION['id']);
+
+if ($_POST) {
+    //Gooit sortingtable leeg
+    GambleManager::ClearSorting();
+    GambleManager::ClearGamble($_SESSION['user']);
+
     for ($i = 1; $i <= 20; $i++) {
-        if(isset($_POST["driver$i"])) {
+        if (isset($_POST["position$i"])) {
 
             GambleManager::CalculateGamble(
-                     $i,
-                    "5",
-                    $_POST["driver$i"],
-                    $_SESSION['id']
+                $i,
+                "4",
+                $_POST["position$i"],
+                $_SESSION['id']
             );
         }
     }
+    header("location: beforegamble.php");
+}
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Gamble - Formula One</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    </head>
-    <body>
-        <form method="POST">
-            <table class="table table-striped">
-                <thead class="table-dark">
-                    <th>Gamble <input type="submit"></th>
-                </thead>
-                <tbody>
-                    <?php
-                        for ($i = 1; $i <= 20; $i++) {
-                            echo "<tr>";
-                                echo "<td>$i: "; 
-                                    echo "<select name='driver$i'>";
-                                        echo "<option hidden none>Chose driver</option>";
-                                        foreach(DriverManager::GetDriver() as $driver) {
-                                            echo "<option value='$driver->driverId'>$driver->driverLastname</option>";
-                                        }
-                                    echo "</select>";
-                                echo "</td>";
-                            echo "</tr>";
-                        }
-                    ?>
-                </tbody>
-            </table>
-        </form>
-    </body>
+
+<head>
+    <title>Gamble - Formula One</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <form method="POST">
+        <table class="table table-striped">
+            <thead class="table-dark">
+                <th>Gamble <input type="submit"></th>
+            </thead>
+            <tbody>
+                <?php
+                for ($i = 1; $i <= 20; $i++) {
+                    echo "<tr>";
+                    echo "<td>position $i: ";
+                    echo "<select name='position$i'>";
+                    echo "<option hidden none>Chose driver</option>";
+                    foreach (DriverManager::GetDriver() as $driver) {
+                        echo "<option value='$driver->driverId'>$driver->driverLastname</option>";
+                    }
+                    echo "</select>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </form>
+</body>
+
 </html>
--- gamble.php
+++ gamble.php
@@ -1,38 +1,62 @@
 <?php
-    require_once "database.php";
-    require_once "managers/GambleManager.php";
+session_start();
 
-    if (isset($_SESSION['id'])) {
-        $personmanager = new personmanager();
-        $person = $personmanager->getAllById($_SESSION['id']);
-    }
+require_once "database.php";
+require_once "managers/GambleManager.php";
+require_once "managers/RaceManger.php";
+require_once "managers/personmanager.php";
+
+$gambles = GambleManager::GetFinalGamble();
+$races = RaceManger::GetRace();
+
+
+
+
+
+
+
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Gamble - Formula One</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    </head>
-    <body>
-        <table class="table table-striped">
-            <thead class="table-dark">
-                <tr>
-                    <td>Position</td>
-                    <td>Username</td>
-                    <td>Points</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach (GambleManager::GetGamble() as $gamble) {
-                        echo "<tr>";
-                            echo "<td>$gamble->gamblePosition</td>";
-                            echo "<td>$gamble->userInfoUsername</td>";
-                            echo "<td>$gamble->gamblePoints</td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </tbody>
-        </table>
-    </body>
+
+<head>
+    <title>Gamble - Formula One</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <select class="m-5" name="racetype">
+        <?php
+        echo "<option hidden none>Chose race</option>";
+        foreach ($races as $race) {
+            echo "<option value='$race->raceId'>$race->raceTrack</option>";
+        }
+        ?>
+    </select>
+
+
+
+
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <td>Race</td>
+                <td>Username</td>
+                <td>Points</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($gambles as $gamble) {
+                echo "<tr>";
+                echo "<td>$gamble->raceTrack</td>";
+                echo "<td>$gamble->userInfoUsername</td>";
+                echo "<td>$gamble->Points</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+
 </html>
